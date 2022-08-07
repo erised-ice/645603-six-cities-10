0dbb3f8d3cef2b83666cd5bd4b62f755ee2f6d09
alexandra <le.onze.mai@gmail.com>
2022-08-08 00:20:44 +0300

    Added reviews type, refactored property screen, implementing reviews loading in progress

--- project/src/pages/property-screen/property-screen.tsx
+++ project/src/pages/property-screen/property-screen.tsx
@@ -2,29 +2,29 @@ import React from 'react';
 import {Navigate, useParams} from 'react-router-dom';
 import ReviewCard from '../../components/review-card/review-card';
 import Header from '../../components/header/header';
-import {Offers} from '../../types/offer';
 import PlacesList from '../../components/places-list/places-list';
 import ReviewForm from '../../components/review-form/review-form';
 import {AppRoute} from '../../const';
+import {useAppSelector} from '../../hooks';
+import {Offer} from '../../types/offer';
 
-type PropertyScreenProps = {
-  offers: Offers;
-}
-
-function PropertyScreen({offers}: PropertyScreenProps): JSX.Element {
+function PropertyScreen(): JSX.Element {
   const params = useParams();
-  const offer = offers.find((item) => item.id.toString() === params.id);
+  const {offers} = useAppSelector((state) => state);
+  const offer = offers.find((item) => item.id.toString() === params.id) as Offer;
+  const {images, title, isFavorite, isPremium, rating, type, bedrooms, maxAdults, price, goods, description, host} = offer;
+  const {name, isPro, avatarUrl} = host;
 
   return (
     <div className="page">
       <Header />
-      {/*TODO: fill all info*/}
       {offer ? (
         <main className="page__main page__main--property">
           <section className="property">
             <div className="property__gallery-container container">
               <div className="property__gallery">
-                {offer.images.map((item) => (
+                {/*TODO: should be not more 6 img*/}
+                {images.map((item) => (
                   <div
                     key={item}
                     className="property__image-wrapper"
@@ -32,7 +32,7 @@ function PropertyScreen({offers}: PropertyScreenProps): JSX.Element {
                     <img
                       className="property__image"
                       src={item}
-                      alt="studio"
+                      alt=""
                     />
                   </div>
                 ))}
@@ -40,95 +40,76 @@ function PropertyScreen({offers}: PropertyScreenProps): JSX.Element {
             </div>
             <div className="property__container container">
               <div className="property__wrapper">
-                {offer.isPremium ? (
+                {isPremium ? (
                   <div className="property__mark">
                     <span>Premium</span>
                   </div>
                 ) : null}
                 <div className="property__name-wrapper">
                   <h1 className="property__name">
-                    {offer.title}
+                    {title}
                   </h1>
-                  <button className={`property__bookmark-button button${offer.isFavorite ? ' property__bookmark-button--active' : ''}`} type="button">
+                  <button className={`property__bookmark-button button${isFavorite ? ' property__bookmark-button--active' : ''}`} type="button">
                     <svg className="property__bookmark-icon" width="31" height="33">
                       <use xlinkHref="#icon-bookmark"></use>
                     </svg>
                     <span className="visually-hidden">To bookmarks</span>
                   </button>
                 </div>
+                {/*TODO: count rating width*/}
                 <div className="property__rating rating">
                   <div className="property__stars rating__stars">
                     <span style={{width: '80%'}}></span>
                     <span className="visually-hidden">Rating</span>
                   </div>
-                  <span className="property__rating-value rating__value">4.8</span>
+                  <span className="property__rating-value rating__value">{rating}</span>
                 </div>
                 <ul className="property__features">
                   <li className="property__feature property__feature--entire">
-                    Apartment
+                    {type}
                   </li>
                   <li className="property__feature property__feature--bedrooms">
-                    3 Bedrooms
+                    {bedrooms} Bedrooms
                   </li>
                   <li className="property__feature property__feature--adults">
-                    Max 4 adults
+                    Max {maxAdults} adults
                   </li>
                 </ul>
                 <div className="property__price">
-                  <b className="property__price-value">&euro;{offer.price}</b>
+                  <b className="property__price-value">&euro;{price}</b>
                   <span className="property__price-text">&nbsp;night</span>
                 </div>
                 <div className="property__inside">
                   <h2 className="property__inside-title">What&apos;s inside</h2>
                   <ul className="property__inside-list">
-                    <li className="property__inside-item">
-                      Wi-Fi
-                    </li>
-                    <li className="property__inside-item">
-                      Washing machine
-                    </li>
-                    <li className="property__inside-item">
-                      Towels
-                    </li>
-                    <li className="property__inside-item">
-                      Heating
-                    </li>
-                    <li className="property__inside-item">
-                      Coffee machine
-                    </li>
-                    <li className="property__inside-item">
-                      Baby seat
-                    </li>
-                    <li className="property__inside-item">
-                      Kitchen
-                    </li>
-                    <li className="property__inside-item">
-                      Dishwasher
-                    </li>
-                    <li className="property__inside-item">
-                      Cabel TV
-                    </li>
-                    <li className="property__inside-item">
-                      Fridge
-                    </li>
+                    {goods.map((good) => (
+                      <li
+                        className="property__inside-item"
+                        key={good}
+                      >
+                        {good}
+                      </li>
+                    ))}
                   </ul>
                 </div>
                 <div className="property__host">
                   <h2 className="property__host-title">Meet the host</h2>
                   <div className="property__host-user user">
-                    <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-                      <img className="property__avatar user__avatar" src="img/avatar-angelina.jpg" width="74" height="74" alt="Host avatar"/>
+                    <div className={`property__avatar-wrapper${isPro ? ' property__avatar-wrapper--pro' : ''} user__avatar-wrapper`}>
+                      <img className="property__avatar user__avatar" src={avatarUrl} width="74" height="74" alt="Host avatar"/>
                     </div>
                     <span className="property__user-name">
-                      Angelina
+                      {name}
                     </span>
-                    <span className="property__user-status">
-                      Pro
-                    </span>
+                    {isPro ? (
+                      <span className="property__user-status">
+                        Pro
+                      </span>
+                    ) : null}
                   </div>
                   <div className="property__description">
                     <p className="property__text">
-                      {offer.description}
+                      {description}
                     </p>
                   </div>
                 </div>
