6097d4565d8ccb6038f22039cd7330e72a25df01
alexandra <le.onze.mai@gmail.com>
2022-08-26 15:30:16 +0300

    Fixes after PR

--- project/src/store/nearby-offers-data/nearby-offers-data.ts
+++ project/src/store/nearby-offers-data/nearby-offers-data.ts
@@ -1,7 +1,7 @@
 import {createSlice} from '@reduxjs/toolkit';
 import {NearbyOffersData} from '../../types/state';
 import {NameSpace} from '../../const';
-import {fetchNearbyOffersAction} from '../api-actions';
+import {changeOfferStatusAction, fetchNearbyOffersAction} from '../api-actions';
 
 const initialState: NearbyOffersData = {
   nearbyOffers: [],
@@ -15,6 +15,13 @@ export const nearbyOffersData = createSlice({
     builder
       .addCase(fetchNearbyOffersAction.fulfilled, (state, action) => {
         state.nearbyOffers = action.payload;
+      })
+      .addCase(changeOfferStatusAction.fulfilled, (state, action) => {
+        const offer = state.nearbyOffers.find((item) => item.id === action.payload.id);
+
+        if (offer) {
+          offer.isFavorite = action.payload.isFavorite;
+        }
       });
   }
 });
